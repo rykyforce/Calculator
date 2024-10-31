dd8a80141b3d9468a53bcec8b45627b8ba870a93
formigz <rformigajruk@gmail.com>
2024-10-31 00:52:54 +0000

    Implemented the Operation state
    TODO: do the help and main state

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,13 +1,16 @@
 ﻿using Calculator.Main.Enumerators;
+using Calculator.Main.Interfaces;
 class Program
 {
     //UI strings to add
     static string ui_line = "######################################################################";
     static string ui_name = "########################";
     static State CurrentState = State.Main;
+    static bool isPlaying = true;
+    
     static void Main() 
     {
-        while (true)
+        while (isPlaying)
         {
             switch (CurrentState)
             {
@@ -34,7 +37,11 @@ class Program
     {
         Header();
         Console.WriteLine("Please select an option:");
-        Console.WriteLine("1. Operation\n2. Help");
+        Console.WriteLine("" +
+            "1. Operation\n" +
+            "2. Help\n" +
+            "3. Exit\n" +
+            "");
         string? response = Console.ReadLine();
         if (response != null) 
         {
@@ -47,8 +54,90 @@ class Program
 
     static void OperationState() 
     {
-    Console.WriteLine("Entered Operation");
-    string? response = Console.ReadLine();
+        //Variables
+        double num1 = 0;
+        double num2 = 0;
+        double result = 0;
+        bool wantToContinue = true;
+        bool madeOperation = false;
+
+        while (wantToContinue) 
+        {
+            Console.Clear();
+            Header();
+            Console.WriteLine("Entered Operation");
+            Console.WriteLine("-> Please enter the operations like: 10 * 10. We only accept one operation at the time\n" +
+                "-> Currently we support: multiplication(*), addition(+), subtraction(-), division(/)\n" +
+                "-> If you wanna go back please write back");
+            string response = Console.ReadLine();
+            if (response != null)
+            {
+                if (response == "back") 
+                {
+                    CurrentState = State.Main;
+                    break;
+                }
+                IOperation selectedOperation;
+                List<string> resArray = response.Split(' ').ToList();
+                if (resArray.Count == 3)
+                {
+                    if (double.TryParse(resArray[0], out num1) && double.TryParse(resArray[2], out num2))
+                    {
+                        switch (resArray[1])
+                        {
+                            case "+":
+                                selectedOperation = OperationFactory.CreateOperation(OperationType.Addition);
+                                result = selectedOperation.Execute(num1, num2);
+                                madeOperation = true;
+                                break;
+                            case "-":
+                                selectedOperation = OperationFactory.CreateOperation(OperationType.Subtraction);
+                                result = selectedOperation.Execute(num1, num2);
+                                madeOperation = true;
+                                break;
+                            case "*":
+                                selectedOperation = OperationFactory.CreateOperation(OperationType.Multiplication);
+                                result = selectedOperation.Execute(num1, num2);
+                                madeOperation = true;
+                                break;
+                            case "/":
+                                selectedOperation = OperationFactory.CreateOperation(OperationType.Division);
+                                result = selectedOperation.Execute(num1, num2);
+                                madeOperation = true;
+                                break;
+                            default:
+                                Console.WriteLine("Selected operation is not on the accepted ones");
+                                Console.WriteLine("Please press any key to try again");
+                                madeOperation = false;
+                                break;
+                        }
+                        if (madeOperation)
+                        {
+                            Console.WriteLine("The result was: " + result);
+                            Console.WriteLine("Please press any key to try again");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.ReadKey();
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong formating please press any key to try again");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong formating please press any key to try again");
+                    Console.ReadKey();
+                }
+            }
+            
+
+        }
     }
 
     static void Header()
